ad45c17d40f2b847a0762e712f4e4e768199005e
Game110AM <Game110AM@605-110-STU12>
2017-04-27 08:34:39 -0600

    hi

---
+++ paxton POKEMON/paxton POKEMON/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void damage();
+void attack();
+int main() {
+	attack();
+	damage();
+	int damage;
+	int level;
+	int power;
+	int a;
+	int d;
+	int modifier;
+}
+void attack() {
+}
+void damage() {
+	damage=2*level/5+2*power*a/d+2/50*modifier 
+}
